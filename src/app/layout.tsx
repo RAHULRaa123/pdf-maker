a3bb12a48262b3e92bc971d6016923765df3b077
RAHULRaa123 <raa426243@gmail.com>
2026-05-25 11:02:18 -0700

    Added manifest link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,16 @@ export default function RootLayout({
           content="ca-pub-9676995453061102"
         />
 
+        {/* PWA Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
